c9ea5bd88c2f39c662425e5dd919914fc64264d9
Everton Cavalcante <evertonranielly@gmail.com>
2022-03-18 02:19:33 -0300

    Revertendo migração de volta para HTML

--- includes/end-scripts.php
+++
@@ -1,8 +0,0 @@
-<!-- JavaScripts -->
-<script src="vendors/@popperjs/popper.min.js"></script>
-<script src="vendors/bootstrap/bootstrap.min.js"></script>
-<script src="vendors/is/is.min.js"></script>
-<script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
-<script src="assets/js/theme.js"></script>
-<script src="vendors/fontawesome/all.min.js"></script>
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet" />
