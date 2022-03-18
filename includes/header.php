7d143de640feac3c3eea2d74cb8c52fed14e66d4
Everton Cavalcante <evertonranielly@gmail.com>
2022-03-18 02:08:18 -0300

    Migr

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!-- Header -->
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+	<link rel="manifest" href="assets/img/favicons/site.webmanifest">
+	
+	<meta name="theme-color" content="#ffffff">
+	<link href="assets/css/theme.css" rel="stylesheet" />
+	
+	<title>MADDIs-EF &ndash; Materiais Digitais Didáticos e Interativos para Educação Física</title>
+</head>
---
+++ includes/nav.php
@@ -0,0 +1,37 @@
+<!-- Navigation bar -->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" data-navbar-on-scroll="data-navbar-on-scroll">
+    <div class="container">
+        <a class="navbar-brand d-flex align-items-center fw-semi-bold fs-3" href="index.html">
+            <img class="me-3" src="assets/img/logos/maddis.svg" alt="" />
+        </a>
+        <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto pt-2 pt-lg-0 font-base">
+                <li class="nav-item px-2" data-anchor="data-anchor"><a class="nav-link fw-medium active" aria-current="page" href="index.html">Início</a></li>
+                <li class="nav-item px-2" data-anchor="data-anchor"><a class="nav-link" href="#">Sobre</a></li>
+                <li class="nav-item px-2" data-anchor="data-anchor"><a class="nav-link" href="#">Pessoas</a></li>
+                <li class="nav-item px-2" data-anchor="data-anchor"><a class="nav-link" href="#">Instituições</a></li>
+                <li class="nav-item px-2" data-anchor="data-anchor"><a class="nav-link" href="#">Pesquisa</a></li>
+                <li class="nav-item px-2" data-anchor="data-anchor"><a class="nav-link" href="#">Publicações</a></li>
+                <div class="dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">Eventos</a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <li><a class="dropdown-item" href="https://cleber-junior.wixsite.com/seminariodolazer/citmef-bra" target="_blank">I CITMEF (2021)</a></li>
+                    </ul>
+                </div>
+                <li class="nav-item px-2" data-anchor="data-anchor"><a class="nav-link" href="#">Notícias</a></li>
+                <li class="nav-item px-2" data-anchor="data-anchor">
+                    <a class="nav-link" href="index.html" role="button"><img class="navbar-img" src="assets/icons/country-flags/brazil.svg" alt="" /></a>
+                </li>
+                <li class="nav-item px-2" data-anchor="data-anchor">
+                    <a class="nav-link" href="#" role="button"><img class="navbar-img" src="assets/icons/en.png" alt="" /></a>
+                </li>
+                <li class="nav-item px-2" data-anchor="data-anchor">
+                    <a class="nav-link" href="#" role="button"><img class="navbar-img" src="assets/icons/country-flags/spain.svg" alt="" /></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
